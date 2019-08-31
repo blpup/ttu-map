2eacad4412afb151340fa5e988b4443832ebfc1d
blpup <arthrjns@outlook.com>
2019-08-31 08:06:40 -0500

    Changed/used a different react map methodology to make stylers work

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,28 +1,97 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import config from './config.js'
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-}
-
-class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(config)
+import _ from "lodash";
+import React from "react";
+import { compose, withProps } from "recompose";
+import config from "./config"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+const styling = [
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   }
+]
 
-  render() {
-    return ( <Map
-            google={this.props.google}
-            zoom={16}
-            style={mapStyles}
-            initialCenter={{ lat: 33.5842591, lng: -101.8804709}}
-          /> );
-  }
 
-}
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      `https://maps.googleapis.com/maps/api/js?key=${config.API_MAP_KEY}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={17} defaultCenter={{ lat: 33.5841916, lng: -101.8748618 }} defaultOptions={{styles: styling}}>
+    <Marker position={{ lat: 33.5841916, lng: -101.8748618 }} />
+  </GoogleMap>
+));
 
-export default GoogleApiWrapper({
-  apiKey: config.API_MAP_KEY
-})(MapContainer);
+const enhance = _.identity;
+
+const ReactGoogleMaps = () => [
+
+
+  <MyMapComponent key="map" />
+];
+
+export default enhance(ReactGoogleMaps);
