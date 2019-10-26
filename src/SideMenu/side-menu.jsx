2270b74af0a33c2afc4255f387ae6320347e2075
blpup <arthrjns@outlook.com>
2019-10-26 12:43:51 -0500

    Made the Sub Menu items into a seperate file and pulled dummy data from json file.

--- src/SideMenu/side-menu.jsx
+++ src/SideMenu/side-menu.jsx
@@ -1,27 +1,11 @@
 import React, {Component, useState} from 'react';
 import styles from './side-menu-styling.css.js';
-import {Collapse} from 'react-bootstrap'
-
+import {Collapse} from 'react-bootstrap';
+import ListItem from './submenu';
 class SideMenu extends Component {
   constructor(props){
     super(props);
     this.state = {open:false};
-    console.log(this.props)
-    this.toggledropDown = this.toggledropDown.bind(this);
-
-    this.items = this.props.data[0].categories.map((item, key) =>
-      <div className="card" key={key}>
-        <a href="#" className="list-group-item list-group-item-action" onClick={this.toggledropDown } aria-controls="food" aria-expanded={this.state.open}>Food</a>
-        <Collapse in={this.state.open}>
-          <div className="card-body" id="food">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-          </div>
-        </Collapse>
-      </div>
-    );
-  }
-  toggledropDown() {
-    this.setState({open:!this.state.open})
   }
   render() {
 
@@ -33,11 +17,9 @@ class SideMenu extends Component {
           <h1>H.U.B</h1>
           <p>This place is cool</p>
           <hr/>
-          <div className="accordion">
-            <ul className="list-group">
-            {this.items}
-            </ul>
-          </div>
+          {
+            this.props.data[0].categories.map((key, index) => <ListItem key={index} data={this.props.data[0].categories[0]}/>)
+          }
         </div>
       </div>
     )
@@ -45,5 +27,3 @@ class SideMenu extends Component {
 }
 
 export default SideMenu;
-
-//// TODO: FIX THE CARDS FROM BEING CLICKED AND ALL DROPING DOWN. PROBABLY NEED SEPERATE STATES.
---
+++ src/SideMenu/submenu.jsx
@@ -0,0 +1,35 @@
+import React, {Component, useState} from 'react';
+import styles from './side-menu-styling.css.js';
+import {Collapse} from 'react-bootstrap'
+
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props)
+    this.toggledropDown = this.toggledropDown.bind(this);
+    this.state = {collapse: false, data: this.props.data};
+  }
+
+toggledropDown() {
+  console.log('in')
+  this.setState({open:!this.state.open})
+}
+
+render() {
+  return (
+    <div className="accordion">
+      <div className="list-group">
+        <div class="card">
+          <a href="#" className="list-group-item list-group-item-action" onClick={this.toggledropDown } aria-controls="food" aria-expanded={this.state.open}>{this.state.data.name}</a>
+          <Collapse in={this.state.open}>
+            <div className="card-body" id="food">
+              {this.state.data.description}
+            </div>
+          </Collapse>
+        </div>
+      </div>
+    </div>
+  )
+}
+}
+export default ListItem;
